11fd02914fc28b62045b11c681248ef50f932313
AdriSaurXYZ <adrianaguilo@paucasesnovescifp.cat>
2023-10-16 16:42:05 +0200

    Push1

---
+++ src/Exercisi2/ExercicisMultiproces2.java
@@ -0,0 +1,29 @@
+package Exercisi2;
+import java.io.*;
+
+public class ExercicisMultiproces2 {
+    public static void main(String[] args) throws IOException {
+        ProcessBuilder pb = new ProcessBuilder("java", "src/Exercisi2/ExercicisMultiproces2_ModificarString.java");
+        Process childProcess = pb.start();
+
+        BufferedReader parentInput = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter parentOutput = new BufferedWriter(new OutputStreamWriter(childProcess.getOutputStream()));
+        BufferedReader childInput = new BufferedReader(new InputStreamReader(childProcess.getInputStream()));
+
+        String line;
+
+        while (true) {
+            System.out.print("El pare diu: ");
+            line = parentInput.readLine();
+            if (line == null || line.equals("exit")) {
+                break;
+            }
+            parentOutput.write(line + "\n");
+            parentOutput.flush();
+            String modifiedLine = childInput.readLine();
+            System.out.println(modifiedLine);
+        }
+
+        childProcess.destroy();
+    }
+}
---
+++ src/Exercisi2/ExercicisMultiproces2_ModificarString.java
@@ -0,0 +1,24 @@
+package Exercisi2;
+import java.io.*;
+
+public class ExercicisMultiproces2_ModificarString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader childInput = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter childOutput = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String line;
+
+        while (true) {
+            System.out.print("El fill diu: ");
+            line = childInput.readLine();
+            if (line == null || line.equals("exit")) {
+                break;
+            }
+
+            // Transformar a majúscules i substituir vocals per "_"
+            line = line.toUpperCase().replaceAll("[AEIOUaeiou]", "_");
+            childOutput.write(line + "\n");
+            childOutput.flush();
+        }
+    }
+}
